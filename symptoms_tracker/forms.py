0039710abb519d6d66503b0f403424bc5c72a6b6
Tori Daughton <toriddaughton@gmail.com>
2022-06-22 22:39:54 -0600

    register / login forms created

--- symptoms_tracker/forms.py
+++ symptoms_tracker/forms.py
@@ -1,16 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateTimeField, SubmitField 
+from wtforms import StringField, PasswordField, DateTimeField, SubmitField, ValidatorError
+from wtforms.validators import DataRequired, Email, EqualTo
+from model import User
 
 # inherit from FlaskForm with newly created form class 
 class RegisterForm(FlaskForm):
 
-    username = StringField("Username: ")
-    email = StringField("Email: ")
-    password = PasswordField("Password: ", [])
-    first_name = StringField("First Name: ")
-    last_name = StringField("Last Name: ")
-    dob = DateTimeField("Date of Birth: ", format='%Y-%m-%d')
-    submit = SubmitField("Submit")
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[ DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
+    pass_confirm = PasswordField('Comfirm Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    dob = DateTimeField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already in use!')
 
-class LoginForm()
+    def check_username(self, field):
+        if User.query.filter_by(username=field).first():
+            raise ValidatorError('Username is taken!')
+        
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validatoris=[DataRequired()])
+    submit = SubmitField('Log In')
--- symptoms_tracker/model.py
+++ symptoms_tracker/model.py
@@ -12,6 +12,9 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 @login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 class User(db.Model, UserMixin):
     
